d4a55dd783a36a9d344dd0aa48312b664fcb1a59
NicoleKio <nastiamay.1654@gmail.com>
2025-01-02 11:06:30 +0200

    master-comm

--- src/main/java/ua/com/nicole/javaspringboot/JavaSpringBootMayApplication.java
+++ src/main/java/ua/com/nicole/javaspringboot/JavaSpringBootMayApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JavaSpringBootMayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavaSpringBootMayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(JavaSpringBootMayApplication.class, args);
+    }
 
 }
