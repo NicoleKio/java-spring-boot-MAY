cad49634c500a45071e8d695a0874d7b652574a9
NicoleKio <nastiamay.1654@gmail.com>
2025-01-02 11:05:20 +0200

    Initial commit

---
+++ src/main/java/ua/com/nicole/javaspringboot/JavaSpringBootMayApplication.java
@@ -0,0 +1,13 @@
+package ua.com.nicole.javaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootMayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBootMayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/com/nicole/javaspringboot/JavaSpringBootMayApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.nicole.javaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringBootMayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
